04713352d2fadb72d1344aef4112ebd53cae0e29
Pabloluis Toyom <pabloluistoyom201830896@cunoc.edu.gt>
2021-06-08 21:58:36 -0600

    Agregue el ejercicio 3 en java

---
+++ Ejercicio3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ejercicio3{
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+
+		int num;
+		int doble, triple;
+
+		Systme.out.println("Ingrese un número");
+		num = entrada.nextInt();
+
+		doble = num *2;
+		triple = num *3;
+
+		System.out.println("El doble del número ingresado es: "+doble);
+		System.out.println("El triple del número ingresado es: "+triple);
+	}
+}
